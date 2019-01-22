b5cf848d2114943cf0547f9ee43305de4c015928
Jhon Jairo Ibarra Samboni <jhonjairo6230@gmail.com>
2019-01-22 18:36:33 -0500

    added mvp design pattern

--- app/src/main/java/com/nokia/test/mobile/view/DessertActivity.java
+++ app/src/main/java/com/nokia/test/mobile/view/DessertActivity.java
@@ -1,7 +1,9 @@
-package com.nokia.test.mobile;
+package com.nokia.test.mobile.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.nokia.test.mobile.R;
 
 public class DessertActivity extends AppCompatActivity {
 
